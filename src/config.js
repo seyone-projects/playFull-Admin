25a7f073b32ba5f34b197433571e473a9d19cb97
Suresh <seyoneprojects@gmail.com>
2025-10-18 09:06:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 // src/config.js
 const applicationMode = "development";
 var appName = "Admin Panel";
-var apiUrl = "http://13.234.225.146:5000/api/";
+var apiUrl = "https://playfulpencil.in/api/";
 var pageSize = 10;
 var logo = "/logo.png";
-var imageBasePath = "http://13.234.225.146:5000/uploads";
+var imageBasePath = "https://playfulpencil.in/uploads";
 
 if (applicationMode === 'development') {
   appName = "Admin Panel";
   
   //local
-  apiUrl = "http://13.234.225.146:5000/api/"; 
-  imageBasePath = "http://13.234.225.146:5000/uploads";  
+  apiUrl = "https://playfulpencil.in/api/"; 
+  imageBasePath = "https://playfulpencil.in/uploads";  
 
   //server
   //apiUrl = "http://13.53.39.107/api/";
